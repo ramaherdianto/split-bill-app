0d85f09d0ae2aa77f745a4c422c8b237f96306f5
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-08-06 16:50:24 +0700

    feat: create add new friend features

--- src/components/form/FormAddFriend.jsx
+++ src/components/form/FormAddFriend.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const FormAddFriend = () => {
+const FormAddFriend = ({ onAddFriend }) => {
+    const [name, setName] = useState('');
+    const [image, setImage] = useState('https://i.pravatar.cc/48');
+
+    const onSubmitFriend = (e) => {
+        e.preventDefault();
+
+        if (!name) return alert('Input field cannot be empty!');
+
+        const id = crypto.randomUUID();
+
+        const newFriend = {
+            id,
+            name,
+            image: `${image}?=${id}`,
+            balance: 0,
+        };
+
+        onAddFriend(newFriend);
+        setName('');
+    };
+
     return (
         <>
-            <form className='form-add-friend'>
+            <form className='form-add-friend' onSubmit={onSubmitFriend}>
                 <label htmlFor=''>🙎🏼Nama</label>
-                <input type='text' />
+                <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
 
-                <label htmlFor=''>📸Gambar</label>
-                <input type='text' />
+                <label style={{ visibility: 'hidden' }} htmlFor=''>
+                    📸Gambar
+                </label>
+                <input
+                    style={{ visibility: 'hidden' }}
+                    type='text'
+                    value={image}
+                    onChange={(e) => setImage(e.target.value)}
+                    disabled
+                />
 
                 <button className='button'>Tambah</button>
             </form>
